6d183680b4b79807bbfd8b47228ef58442a908e7
Ben Brumm <ben@databasestar.com>
2022-09-07 10:30:04 +1000

    Updated oracle scripts

---
+++ sample_db_olympics/oracle/03_games.sql
@@ -0,0 +1,71 @@
+
+
+DROP TABLE IF EXISTS games;
+
+CREATE TABLE games (
+  id NUMBER(11) NOT NULL,
+  games_year NUMBER(11) DEFAULT NULL,
+  games_name varchar2(100) DEFAULT NULL,
+  season varchar2(100) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+
+
+
+
+
+INSERT ALL
+INTO games (id, games_year, games_name, season) VALUES (1,1992,'1992 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (2,2012,'2012 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (3,1920,'1920 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (4,1900,'1900 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (5,1988,'1988 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (6,1992,'1992 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (7,1994,'1994 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (8,1932,'1932 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (9,2002,'2002 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (10,1952,'1952 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (11,1980,'1980 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (12,2000,'2000 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (13,1996,'1996 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (14,1912,'1912 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (15,1924,'1924 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (16,2014,'2014 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (17,1948,'1948 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (18,1998,'1998 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (19,2006,'2006 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (20,2008,'2008 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (21,2016,'2016 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (22,2004,'2004 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (23,1960,'1960 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (24,1964,'1964 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (25,1984,'1984 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (26,1984,'1984 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (27,1968,'1968 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (28,1972,'1972 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (29,1988,'1988 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (30,1936,'1936 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (31,1952,'1952 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (32,1956,'1956 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (33,1956,'1956 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (34,1960,'1960 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (35,1928,'1928 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (36,1976,'1976 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (37,1980,'1980 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (38,1964,'1964 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (39,2010,'2010 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (40,1968,'1968 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (41,1972,'1972 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (42,1976,'1976 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (43,1924,'1924 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (44,1904,'1904 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (45,1906,'1906 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (46,1928,'1928 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (47,1908,'1908 Summer','Summer')
+INTO games (id, games_year, games_name, season) VALUES (48,1948,'1948 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (49,1932,'1932 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (50,1936,'1936 Winter','Winter')
+INTO games (id, games_year, games_name, season) VALUES (51,1896,'1896 Summer','Summer')
+SELECT * FROM dual;
+
