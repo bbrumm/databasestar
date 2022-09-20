16766655a512dd79d46f738663d28506191cb0bc
Ben Brumm <ben@databasestar.com>
2022-09-20 14:55:48 +1000

    Added sample script for Insert guide

---
+++ posts/sample_data_insert.sql
@@ -0,0 +1,14 @@
+/*
+Sample table for my Insert guide.
+Post here: https://www.databasestar.com/sql-insert-into/
+*/
+
+CREATE TABLE student (
+    student_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    fees_required INT,
+    fees_paid INT,
+    enrolment_date DATE,
+    gender VARCHAR(1)
+);
