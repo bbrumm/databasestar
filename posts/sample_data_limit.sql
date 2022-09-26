89d20219d429d4fd4d55cb4a15c2bbe90a7ba757
Ben Brumm <ben@databasestar.com>
2022-09-26 15:11:48 +1000

    Added limit sample script

---
+++ posts/sample_data_limit.sql
@@ -0,0 +1,23 @@
+/*
+Sample data for the SQL Limit post.
+Post here: https://www.databasestar.com/sql-limit/
+*/
+
+CREATE TABLE customer_revenue (
+  customer_id NUMBER(4),
+  revenue NUMBER(10)
+);
+
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (1, 109470);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (2, 9384760);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (3, 1852828);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (4, 3596297);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (5, 82495);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (6, 935191);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (7, 654796);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (8, 4341421);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (9, 3378075);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (10, 5131750);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (11, 4431791);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (12, 245583);
+INSERT INTO customer_revenue (customer_id, revenue) VALUES (13, 3596297);
