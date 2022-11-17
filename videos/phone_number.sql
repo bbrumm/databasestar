23618be3a0c7db9d2be77cffcdc725c59ef9da3c
Ben Brumm <ben@databasestar.com>
2022-11-17 15:11:27 +1100

    Added more to phone number script

--- videos/phone_number.sql
+++ videos/phone_number.sql
@@ -18,3 +18,12 @@ CREATE TABLE customer (
   customer_name VARCHAR(100),
   phone_number VARCHAR(25)
 );
+
+INSERT INTO customer (customer_name, phone_number)
+VALUES ('John Smith', '9876543210');
+
+INSERT INTO customer (customer_name, phone_number)
+VALUES ('Mary Jones', '0621092131');
+
+SELECT customer_name, phone_number
+FROM customer;
