a4d2d3714dc4cc8f6755bf39850c87310c32fae8
Ben Brumm <ben@databasestar.com>
2023-03-08 10:28:26 +1100

    Added update script

---
+++ courses/course_create_second_table_2023/04_update.sql
@@ -0,0 +1,28 @@
+/*
+Update product table
+Oracle, SQL Server, MySQL, Postgres
+*/
+
+UPDATE product
+SET supplier_name = 'Nike'
+WHERE product_name = 'Black and white running shoes';
+
+UPDATE product
+SET supplier_name = 'Adidas'
+WHERE product_name = 'Red and black running shoes';
+
+UPDATE product
+SET supplier_name = 'Smith'
+WHERE product_name = 'Jumper';
+
+UPDATE product
+SET supplier_name = 'Ralph Lauren'
+WHERE product_name = 'Shirt';
+
+UPDATE product
+SET supplier_name = 'Smith'
+WHERE product_name = 'Black dress';
+
+UPDATE product
+SET supplier_name = 'Hush'
+WHERE product_name = 'Slippers';
