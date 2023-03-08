84908c07e8b36d1bb48e54221ff2cccdf3459c7e
Ben Brumm <ben@databasestar.com>
2023-03-08 13:28:54 +1100

    Added script for lesson 5

---
+++ courses/course_create_second_table_2023/05_supplier_oracle.sql
@@ -0,0 +1,28 @@
+/*
+Create and populate the supplier table
+Oracle
+*/
+
+CREATE TABLE supplier (
+  supplier_name VARCHAR2(200),
+  city VARCHAR2(200),
+  num_employees NUMBER(8)
+);
+
+
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Smith', 'Chicago', 20);
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Hush', null, 150);
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Ralph Lauren', 'Portland', null);
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Nike', 'New York City', 3200);
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Adidas', 'Miami', 15);
+INSERT INTO supplier (supplier_name, city, num_employees)
+VALUES ('Harrods', 'Miami', 80);
+
+
+SELECT company_name, city, num_employees
+FROM company;
