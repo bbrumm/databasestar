8c3293cb3840477f1152ebedc6dab48554308775
Ben Brumm <ben@databasestar.com>
2023-03-27 15:20:18 +1100

    Fixed interaction reference

--- courses/course_soft_delete/lesson_05_oracle.sql
+++ courses/course_soft_delete/lesson_05_oracle.sql
@@ -3,12 +3,12 @@ Lesson 05
 Oracle
 */
 
-CREATE VIEW interactions_view AS
+CREATE VIEW interaction_view AS
 SELECT id, interaction_datetime, interaction_type, person_id, notes, active
 FROM interaction;
 
-CREATE OR REPLACE TRIGGER soft_delete_interactions
-INSTEAD OF DELETE ON interactions_view
+CREATE OR REPLACE TRIGGER soft_delete_interaction
+INSTEAD OF DELETE ON interaction_view
 FOR EACH ROW
 BEGIN
 	UPDATE interaction SET active = 0 WHERE id = :old.id;
