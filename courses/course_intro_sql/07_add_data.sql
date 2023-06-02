bb4c76b7160ee65b1fe1528254e2e3c2b707f590
Ben Brumm <ben@databasestar.com>
2023-06-02 15:18:52 +1000

    Added scripts for Intro to SQL course

---
+++ courses/course_intro_sql/07_add_data.sql
@@ -0,0 +1,12 @@
+/*
+Lesson 07
+*/
+
+INSERT INTO product (product_name, price)
+VALUES ('Shirt', 50);
+
+INSERT INTO product (product_name, price)
+VALUES ('Jeans', 200);
+
+INSERT INTO product (product_name)
+VALUES ('Hat');
---
+++ courses/course_intro_sql/11_delete_data.sql
@@ -0,0 +1,9 @@
+/*
+Lesson 11
+*/
+
+DELETE FROM product
+WHERE product_name = 'Shirt';
+
+SELECT product_name, price
+FROM product;
