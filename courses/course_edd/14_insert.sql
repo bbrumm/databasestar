2ed493ac8e7caf408941f7c03a0ce3d03092a34f
Ben Brumm <ben@databasestar.com>
2023-07-08 14:11:13 +1000

    Added scripts for EDD

---
+++ courses/course_edd/14_insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO list (id, list_name) VALUES
+(1, 'To Read'),
+(2, 'Currently Reading'),
+(3, 'Have Read');
+
+INSERT INTO review (id, book_id, rating, review, date_reviewed) VALUES
+(1, NULL, 3, 'This was a good book', 20230410);
+
+INSERT INTO genre (id, genre_name) VALUES
+(1, 'Sci Fi'),
+(2, 'Romance'),
+(3, 'Adventure');
+
+/*
+add book insert
+*/
