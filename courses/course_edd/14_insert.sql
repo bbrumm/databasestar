c10ab16e6cc610fdfc5946a42eb6ee6daaa156ff
Ben Brumm <ben@databasestar.com>
2023-07-10 13:43:53 +1000

    Updated insert

--- courses/course_edd/14_insert.sql
+++ courses/course_edd/14_insert.sql
@@ -7,8 +7,6 @@ INSERT INTO list (id, list_name) VALUES
 (2, 'Currently Reading'),
 (3, 'Have Read');
 
-INSERT INTO review (id, book_id, rating, review, date_reviewed) VALUES
-(1, NULL, 3, 'This was a good book', 20230410);
 
 INSERT INTO genre (id, genre_name) VALUES
 (1, 'Sci Fi'),
@@ -41,3 +39,6 @@ INSERT INTO book (id, list_id, title, author, isbn, date_read) VALUES
 (12, 1, 'Identity', 'Nora Roberts', '34138196', NULL);
 INSERT INTO book (id, list_id, title, author, isbn, date_read) VALUES
 (13, NULL, 'A Court of Thorns and Roses', 'Sarah J Maas', '15183613', NULL);
+
+INSERT INTO review (id, book_id, rating, review, date_reviewed) VALUES
+(1, 2, 3, 'This was a good book', 20230410);
