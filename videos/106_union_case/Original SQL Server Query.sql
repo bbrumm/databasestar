e7ff18192ad3e85ab1373bc0f9be77c9914e722e
Ben Brumm <ben@databasestar.com>
2023-09-21 14:06:48 +1000

    Fixed year formatting

--- videos/106_union_case/Original SQL Server Query.sql
+++ videos/106_union_case/Original SQL Server Query.sql
@@ -9,7 +9,7 @@ FROM (
     SELECT
     ROUND(AVG(sub.val_count),2) AS avg_val
     FROM (
-        SELECT FORMAT(order_date, 'YYYY-MM') AS order_month,
+        SELECT FORMAT(order_date, 'yyyy-MM') AS order_month,
         COUNT(*) AS val_count
         FROM customer_order c
         WHERE order_id IN (
@@ -19,7 +19,7 @@ FROM (
             WHERE p.product_category_id = 5
             AND p.status = 'A'
         )
-        GROUP BY FORMAT(order_date,'YYYY-MM')
+        GROUP BY FORMAT(order_date,'yyyy-MM')
     ) AS sub
 ) AS d         
 UNION ALL
@@ -30,7 +30,7 @@ FROM (
     SELECT 
     ROUND(AVG(sub.val_count),2) AS avg_val
     FROM (
-        SELECT FORMAT(order_date, 'YYYY-MM') AS order_month,
+        SELECT FORMAT(order_date, 'yyyy-MM') AS order_month,
         COUNT(*) AS val_count
         FROM customer_order c
         WHERE order_id IN (
@@ -40,7 +40,7 @@ FROM (
             WHERE p.product_category_id = 1
             AND p.retail_price > 15
         )
-        GROUP BY FORMAT(order_date,'YYYY-MM')
+        GROUP BY FORMAT(order_date,'yyyy-MM')
     ) AS sub
 ) AS d
 UNION ALL
@@ -51,7 +51,7 @@ FROM (
     SELECT 
     ROUND(AVG(sub.val_count),2) AS avg_val
     FROM (
-        SELECT FORMAT(order_date, 'YYYY-MM') AS order_month,
+        SELECT FORMAT(order_date, 'yyyy-MM') AS order_month,
         COUNT(*) AS val_count
         FROM customer_order c
         WHERE order_id IN (
@@ -61,7 +61,7 @@ FROM (
             WHERE p.product_category_id = 3
             AND p.product_name LIKE '%Pasta%'
         )
-        GROUP BY FORMAT(order_date,'YYYY-MM')
+        GROUP BY FORMAT(order_date,'yyyy-MM')
     ) AS sub
 ) AS d
 ORDER BY 1;
