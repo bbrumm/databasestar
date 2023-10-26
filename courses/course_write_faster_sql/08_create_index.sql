eae5e0c0328ce4419c11195a35ee3bebc4af3b66
Ben Brumm <ben@databasestar.com>
2023-10-27 09:14:02 +1100

    Updated index query

--- courses/course_write_faster_sql/08_create_index.sql
+++ courses/course_write_faster_sql/08_create_index.sql
@@ -9,7 +9,3 @@ FROM book b
 INNER JOIN publisher p ON b.publisher_id = p.publisher_id
 WHERE b.publisher_id = 1149;
 
-CREATE INDEX idx_book_pubid
-ON book (publisher_id);
-
-DROP INDEX idx_book_pubid;
