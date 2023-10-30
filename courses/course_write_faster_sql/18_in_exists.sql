18d08ebdc4331a1bcf3f4e361958336d668ece29
Ben Brumm <ben@databasestar.com>
2023-10-30 12:19:53 +1100

    Updated script 18

--- courses/course_write_faster_sql/18_in_exists.sql
+++ courses/course_write_faster_sql/18_in_exists.sql
@@ -1,6 +1,5 @@
 /*
 Original query
-Cost in MySQL 4,344
 */
 SELECT
 b.book_id,
@@ -12,14 +11,15 @@ WHERE b.book_id IN (
 	SELECT book_id
     FROM order_line
     WHERE price > (
-        SELECT AVG(price)
+        SELECT AVG(price) AS avg_price
 		FROM order_line
     )
 );
 
+
+
 /*
 Change to Exists
-Cost: 4,344
 */
 SELECT
 b.book_id,
@@ -29,9 +29,9 @@ b.num_pages
 FROM book b
 WHERE EXISTS (
 	SELECT 1
-    FROM order_line
-    WHERE book_id = b.book_id
-    AND price > (
+    FROM order_line ol
+    WHERE ol.book_id = b.book_id
+    AND ol.price > (
         SELECT AVG(price)
 		FROM order_line
     )
