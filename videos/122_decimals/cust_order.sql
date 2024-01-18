ba2c2e86b033087d8710c6ef47f8342a244d40db
Ben Brumm <ben@databasestar.com>
2024-01-18 15:26:31 +1100

    Added new queries

--- videos/122_decimals/cust_order.sql
+++ videos/122_decimals/cust_order.sql
@@ -1,11 +1,20 @@
+DROP TABLE cust_order;
+
 CREATE TABLE cust_order (
   order_id INT,
   customer_id INT,
-  total_order_amount DECIMAL(8, 2)
+  total_order_amount INT
 );
 
 INSERT INTO cust_order (order_id, customer_id, total_order_amount) VALUES
-(1, 4, 128.45),
-(2, 18, 302.99),
-(3, 2, 683.43),
-(4, 13, 28.26);
+(1, 4, 12845),
+(2, 18, 30299),
+(3, 2, 68343),
+(4, 13, 2826);
+
+SELECT
+order_id,
+customer_id,
+total_order_amount,
+total_order_amount / 100 AS amount_dollars
+FROM cust_order;
