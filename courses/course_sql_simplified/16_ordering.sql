889a3554e3769855592e18b42ecef095060fb154
Ben Brumm <ben@databasestar.com>
2024-04-08 12:28:19 +1000

    Added script 17

--- courses/course_sql_simplified/16_ordering.sql
+++ courses/course_sql_simplified/16_ordering.sql
@@ -1,3 +1,37 @@
 /*
 Lesson 16
 */
+
+-- SQL 01
+SELECT product_name, price
+FROM product;
+
+-- SQL 02
+SELECT product_name, price
+FROM product
+ORDER BY price ASC;
+
+-- SQL 03
+SELECT product_name, price
+FROM product
+ORDER BY price DESC;
+
+-- SQL 04
+SELECT product_name, price
+FROM product
+ORDER BY product_name ASC;
+
+-- SQL 05
+SELECT product_name, price
+FROM product
+ORDER BY 2 ASC;
+
+-- SQL 06
+SELECT price. product_name
+FROM product
+ORDER BY 2 ASC;
+
+-- SQL 07
+SELECT product_name, price
+FROM product
+ORDER BY price ASC, product_name ASC;
