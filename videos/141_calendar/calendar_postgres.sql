a083f5cfeab46cd8d29d64e8c3cca4a6f8e099bf
Ben Brumm <ben@databasestar.com>
2024-05-14 15:36:48 +1000

    First calendar table in Postgres

---
+++ videos/141_calendar/calendar_postgres.sql
@@ -0,0 +1,23 @@
+/*
+Postgres
+Create the days table
+*/
+
+CREATE TABLE calendar_days (
+  day_id INT,
+  calendar_date DATE,
+  calendar_year SMALLINT,
+  calendar_quarter SMALLINT,
+  calendar_quarter_name VARCHAR(10),
+  calendar_month SMALLINT,
+  calendar_month_longname VARCHAR(20),
+  calendar_month_shortname VARCHAR(5),
+  calendar_week_of_year SMALLINT,
+  calendar_day_of_year SMALLINT,
+  calendar_day_longname VARCHAR(20),
+  calendar_day_shortname VARCHAR(5),
+  is_weekend SMALLINT,
+  is_holiday SMALLINT,
+  is_workday SMALLINT,
+  CONSTRAINT pk_calendar PRIMARY KEY (day_id)
+);
