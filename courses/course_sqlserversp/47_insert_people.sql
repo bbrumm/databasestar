bb9ab3b5f27dcdff3ef3258a6b4856ef2f2eaa54
Ben Brumm <ben@databasestar.com>
2024-06-03 11:43:36 +1000

    Updated lesson 47

--- courses/course_sqlserversp/47_insert_people.sql
+++ courses/course_sqlserversp/47_insert_people.sql
@@ -1,2 +1,14 @@
-INSERT INTO Application.People ()
-VALUES ();
+INSERT INTO Application.People (FullName, PreferredName, IsPermittedToLogon,
+LogonName, IsExternalLogonProvider, HashedPassword, IsSystemUser, IsEmployee,
+IsSalesperson, UserPreferences, PhoneNumber, FaxNumber, EmailAddress, 
+Photo, CustomFields, LastEditedBy)
+VALUES ('Sarah Jones', 'Sarah', 0, 'NO LOGON', 0, NULL, 0, 0, 0,
+NULL, NULL, NULL, 'sarah@test.com', NULL, NULL, 1);
+
+
+SELECT *
+FROM Application.People
+ORDER BY PersonID DESC;
+
+SELECT *
+FROM PersonActivity;
