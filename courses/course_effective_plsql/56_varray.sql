5cd33dda0cdf81c75363a2c01031940152779955
Ben Brumm <ben@databasestar.com>
2024-07-16 08:15:09 +1000

    Updated varray script

--- courses/course_effective_plsql/56_varray.sql
+++ courses/course_effective_plsql/56_varray.sql
@@ -1,10 +1,5 @@
 CREATE OR REPLACE PROCEDURE PrintEmployeeStatuses
 AS
-  v_status_1 VARCHAR2(20) := 'Active';
-  v_status_2 VARCHAR2(20) := 'Past';
-  v_status_3 VARCHAR2(20) := 'Hire Pending';
-  v_status_4 VARCHAR2(20) := 'Exiting';
-  
   TYPE varray_emp_status IS VARRAY(4) OF VARCHAR2(20);
   v_emp_status varray_emp_status;
 BEGIN
