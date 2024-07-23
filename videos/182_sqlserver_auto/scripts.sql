91c9cfb53746b6ffc7f425ce088d56fd24396712
Ben Brumm <ben@databasestar.com>
2024-07-23 16:08:27 +1000

    Added scripts for SQL Server import

---
+++ videos/182_sqlserver_auto/scripts.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE cust_orders (
+  cust_id INTEGER,
+  order_date DATE,
+  order_amount INTEGER
+);
+
+
+BULK INSERT dbo.cust_orders
+FROM 'C:/Users/bbrum/databasestar/databasestar/videos/182_sqlserver_auto/cust_orders.csv'
+WITH
+(
+  FIELDTERMINATOR =',',
+  ROWTERMINATOR = '0x0a'
+);
