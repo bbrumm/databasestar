2066212f9551cf08fea2ec194b347daf1e1067b2
Ben Brumm <ben@databasestar.com>
2024-07-26 11:11:12 +1000

    Added MySQL query for the start

--- videos/186_100m_mysql/mysql_import.sql
+++ videos/186_100m_mysql/mysql_import.sql
@@ -15,18 +15,18 @@ CREATE TABLE steam_reviews (
   author_playtime_at_review INT,
   author_last_played INT,
   language VARCHAR(500),
-  review VARCHAR(6000),
+  review VARCHAR(8000),
   timestamp_created VARCHAR(100),
   timestamp_updated VARCHAR(100),
-  voted_up INT,
-  votes_up INT,
-  votes_funny INT,
+  voted_up BIGINT,
+  votes_up BIGINT,
+  votes_funny BIGINT,
   weighted_vote_score INT,
   comment_count INT,
   steam_purchase INT,
   received_for_free INT,
   written_during_early_access INT,
-  hidden_in_steam_china INT,
+  hidden_in_steam_china VARCHAR(10),
   steam_china_location VARCHAR(500)
 );
 
@@ -34,9 +34,12 @@ TRUNCATE TABLE steam_reviews;
 
 /*
 Import the CSV file
+1m rows - takes 12 sec
+10m rows - takes 128 seconds
+100m+ rows - takes X seconds
 */
 LOAD DATA LOCAL INFILE  
-'/Users/BB/Downloads/small_reviews.csv'
+'/Users/BB/Downloads/reviews_10m.csv'
 INTO TABLE steam_reviews  
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
@@ -56,10 +59,3 @@ IGNORE 1 LINES
 
 SELECT *
 FROM steam_reviews;
-
-
-SELECT *
-FROM steam_reviews
-WHERE recommendationid = 148919350;
-
-
--- videos/186_100m_mysql/mysql_queries.sql
+++ videos/186_100m_mysql/mysql_queries.sql
@@ -1,13 +1,63 @@
 SELECT *
 FROM steam_reviews;
 
+
+
 /*
-Query for analysis
+Main query
+The starting point of the video
+This shows a cost of 13,251 with the medium_reviews file (approx 100k rows)
+For 1m rows, cost is 142,267, time taken is 13 seconds
+For 10m rows, cost is 1,533,828, time taken is 167 seconds
+For 100m+ rows, cost is X, time taken is X
 */
 
 SELECT
-game,
-FROM_UNIXTIME(timestamp_created) AS created_date,
-votes_up
+game, 
+recommendationid,
+review,
+votes_up,
+FROM_UNIXTIME(timestamp_created) AS created_date
 FROM steam_reviews
-WHERE written_during_early_access = 0;
+WHERE (
+	author_steamid IN (
+        #Find the author IDs of the top 10% of authors based on the number of games owned
+		SELECT
+		author_steamid
+		FROM (
+			SELECT
+			author_steamid,
+			PERCENT_RANK () OVER (ORDER BY author_num_games_owned DESC) AS rank_games_owned
+			FROM steam_reviews
+		) s
+		WHERE s.rank_games_owned <= 0.10
+	)
+	OR author_steamid IN (
+		#Find the author IDs of the top 10% of authors based on play time
+		SELECT
+		author_steamid
+		FROM (
+			SELECT
+			author_steamid,
+			PERCENT_RANK () OVER (ORDER BY author_playtime_forever DESC) AS rank_playtime
+			FROM steam_reviews
+		) s
+		WHERE s.rank_playtime <= 0.10
+	)
+    OR author_steamid IN (
+		#Find the author IDs of the top 10% of authors based on number of reviews
+		SELECT
+		author_steamid
+		FROM (
+			SELECT
+			author_steamid,
+			PERCENT_RANK () OVER (ORDER BY author_num_reviews DESC) AS rank_num_reviews
+			FROM steam_reviews
+		) s
+		WHERE s.rank_num_reviews <= 0.10
+  )
+)
+AND votes_up > 0
+AND language = 'english'
+ORDER BY votes_up DESC;
+
