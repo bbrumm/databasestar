f7c7890a91e504910961eb83f2bbc81a00c331a7
Ben Brumm <ben@databasestar.com>
2024-07-31 15:53:39 +1000

    Module 2 added

---
+++ courses/course_postgres_mastery/15_frame_start.sql
@@ -0,0 +1,96 @@
+/*
+Frame Start
+*/
+
+--SQL 01
+SELECT
+co.order_id,
+co.order_date,
+b.title,
+ol.price,
+MAX(ol.price) OVER () AS max_book_price
+FROM cust_order co 
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+INNER JOIN book b ON ol.book_id = b.book_id 
+ORDER BY co.order_date ASC, co.order_id ASC;
+
+--SQL 02
+SELECT
+co.order_id,
+co.order_date,
+b.title,
+ol.price,
+MAX(ol.price) OVER (ORDER BY co.order_date) AS max_book_price
+FROM cust_order co 
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+INNER JOIN book b ON ol.book_id = b.book_id 
+ORDER BY co.order_date ASC, co.order_id, ol.price ASC;
+
+
+--SQL 03
+SELECT
+co.order_id,
+co.order_date,
+b.title,
+ol.price,
+MAX(ol.price) OVER (ORDER BY co.order_date RANGE UNBOUNDED PRECEDING) AS max_book_price
+FROM cust_order co 
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+INNER JOIN book b ON ol.book_id = b.book_id 
+ORDER BY co.order_date ASC, co.order_id, ol.price ASC;
+
+--SQL 04
+SELECT
+co.order_id,
+co.order_date,
+b.title,
+ol.price,
+MAX(ol.price) OVER (ORDER BY co.order_date ROWS UNBOUNDED PRECEDING) AS max_book_price
+FROM cust_order co 
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+INNER JOIN book b ON ol.book_id = b.book_id 
+ORDER BY co.order_date ASC, co.order_id, ol.price ASC;
+
+
+--SQL 05
+SELECT
+co.order_id,
+co.order_date,
+b.title,
+ol.price,
+MAX(ol.price) OVER (ORDER BY co.order_date ROWS 5 PRECEDING) AS max_book_price
+FROM cust_order co 
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+INNER JOIN book b ON ol.book_id = b.book_id 
+ORDER BY co.order_date ASC, co.order_id, ol.price ASC;
+
+
+--SQL 06
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
+
+--SQL 07
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date ROWS 5 PRECEDING)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
+
+--SQL 08
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date RANGE INTERVAL '7' DAY PRECEDING)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
---
+++ courses/course_postgres_mastery/16_frame_between.sql
@@ -0,0 +1,33 @@
+/*
+Frame Between
+*/
+
+--SQL 01
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
+
+--SQL 02
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date ROWS BETWEEN 3 PRECEDING AND 3 FOLLOWING)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
+
+--SQL 03
+SELECT
+order_date::date,
+COUNT(*) AS order_line_count,
+SUM(COUNT(*)) OVER(ORDER BY order_date::date ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+FROM cust_order co
+INNER JOIN order_line ol ON co.order_id = ol.order_id
+GROUP BY order_date::date
+ORDER BY order_date::date ASC;
