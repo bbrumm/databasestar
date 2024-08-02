8f633c6ca7218b34dde3bc43e8a9539c103f4d96
Ben Brumm <ben@databasestar.com>
2024-08-02 15:50:54 +1000

    More JSON files

---
+++ courses/course_postgres_mastery/41_build_object.sql
@@ -0,0 +1,26 @@
+/*
+Build Object
+*/
+
+--SQL 01
+SELECT JSONB_BUILD_OBJECT('id', 4, 'name', 'Jacket');
+
+--SQL 02
+INSERT INTO test_jsonb (id, some_data)
+VALUES (7, JSONB_BUILD_OBJECT('id', 4, 'name', 'Jacket'));
+
+--SQL 03
+SELECT id, some_data
+FROM test_jsonb;
+
+--SQL 04
+SELECT JSONB_BUILD_OBJECT('id', 5, 'name', 'Shoes', 'price');
+
+--SQL 05
+SELECT
+method_id,
+method_name,
+cost,
+JSON_BUILD_OBJECT('method_id', method_id, 'method_name', method_name, 'cost', cost) AS row_as_json
+FROM shipping_method;
+
---
+++ courses/course_postgres_mastery/42_other_functions.sql
@@ -0,0 +1,33 @@
+/*
+Other Functions
+*/
+
+--SQL 01
+SELECT
+method_name,
+TO_JSON(method_name),
+TO_JSONB(method_name)
+FROM shipping_method;
+
+--SQL 02
+SELECT
+method_id,
+method_name,
+cost 
+FROM shipping_method;
+
+--SQL 03
+SELECT
+ROW_TO_JSON(sm) AS shipping_records
+FROM (
+  SELECT method_id, method_name, cost
+  FROM shipping_method
+) sm;
+
+--SQL 04
+SELECT
+JSON_BUILD_ARRAY(1, 3, 78, 12);
+
+--SQL 05
+SELECT id, some_data, JSONB_PRETTY(some_data) AS formatted_json
+FROM test_jsonb;
