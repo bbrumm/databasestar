fcee5509fc54e221492f316aeab5d192ad5f4016
Ben Brumm <ben@databasestar.com>
2024-08-08 12:18:35 +1000

    Updated row number

--- courses/course_postgres_mastery/12_rownumber.sql
+++ courses/course_postgres_mastery/12_rownumber.sql
@@ -12,7 +12,7 @@ ROW_NUMBER() OVER(ORDER BY SUM(ol.price) DESC) AS order_total_rank
 FROM cust_order co 
 INNER JOIN shipping_method sm ON co.shipping_method_id = sm.method_id 
 INNER JOIN order_line ol ON co.order_id = ol.order_id 
-GROUP BY co.order_id 
+GROUP BY co.order_id, co.order_date
 ORDER BY SUM(ol.price) DESC;
 
 
@@ -28,5 +28,5 @@ ROW_NUMBER() OVER(ORDER BY SUM(ol.price) DESC) AS order_total_rownumber
 FROM cust_order co 
 INNER JOIN shipping_method sm ON co.shipping_method_id = sm.method_id 
 INNER JOIN order_line ol ON co.order_id = ol.order_id 
-GROUP BY co.order_id 
+GROUP BY co.order_id, co.order_date
 ORDER BY SUM(ol.price) DESC;
