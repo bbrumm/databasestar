97f6695bf03de06724c92dc30764bc60b8db5536
Ben Brumm <ben@databasestar.com>
2024-08-15 14:10:22 +1000

    Updated DDL script

--- courses/course_postgres_mastery/59_ddl_t1.sql
+++ courses/course_postgres_mastery/59_ddl_t1.sql
@@ -3,16 +3,23 @@ DDL in Transactions - Transaction 1
 */
 
 --SQL 01
+START TRANSACTION;
+
+--SQL 02
 CREATE TABLE my_new_table (
 	id INTEGER
 );
 
---SQL 02
+--SQL 03
 INSERT INTO my_new_table (id) VALUES (1);
 
---SQL 03
+--SQL 04
 SELECT id
 FROM my_new_table;
 
---SQL 04
-COMMIT;
+--SQL 05
+ROLLBACK;
+
+--SQL 06
+SELECT id
+FROM my_new_table;
