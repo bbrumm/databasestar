93e286220179e3008dba13bac8880cc1a32f191d
Ben Brumm <ben@databasestar.com>
2024-08-16 19:43:29 +1000

    Fixed JSONB_SET

--- courses/course_postgres_mastery/46_update_json.sql
+++ courses/course_postgres_mastery/46_update_json.sql
@@ -13,10 +13,10 @@ WHERE publisher_id = 15;
 
 --SQL 03
 UPDATE publisher_contacts
-SET publisher_details = JSONB_SET(publisher_details -> 'contacts' -> 0, '{first_name}', '"Thomas"')
+SET publisher_details = JSONB_SET(publisher_details, '{contacts, 0, first_name}', '"Thomas"')
 WHERE publisher_id = 15;
 
 --SQL 04
 UPDATE publisher_contacts
-SET publisher_details = JSONB_SET(publisher_details -> 'contacts' -> 0, '{status}', '"active"')
+SET publisher_details = JSONB_SET(publisher_details -> '{contacts, 0, status}', '"active"')
 WHERE publisher_id = 15;
