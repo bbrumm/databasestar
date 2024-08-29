2b67c34ab0b3285f66d5cf747a6b79681a92627c
Ben Brumm <ben@databasestar.com>
2024-08-29 14:44:34 +1000

    Updates for SQL Server

--- videos/187_generate_millions/generate_sqlserver.sql
+++ videos/187_generate_millions/generate_sqlserver.sql
@@ -1,5 +1,6 @@
 DROP TABLE test_data;
 
+--SQL 01
 CREATE TABLE test_data (
   test_num INT,
   test_text VARCHAR(100),
@@ -11,10 +12,12 @@ FROM test_data;
 
 TRUNCATE TABLE test_data;
 
+--SQL 02
 SELECT
 FLOOR(RAND() * 1000000) AS test_num,
 SUBSTRING(CONVERT(NVARCHAR(32), HashBytes('MD5', STR(RAND() * 1000000)), 2), 1, 20) AS test_text,
 DATEADD(day, FLOOR(RAND() * 365 * 20), '2000-01-01') AS test_date;
+
 
 INSERT INTO test_data (test_num, test_text, test_datetime)
 SELECT
@@ -27,6 +30,7 @@ Stored procedure
 */
 GO;
 
+--SQL 03
 CREATE OR ALTER PROCEDURE generate_sample_data
   @number_of_rows INT
 AS
@@ -37,9 +41,9 @@ BEGIN
   BEGIN
     INSERT INTO test_data (test_num, test_text, test_datetime)
     SELECT
-	FLOOR(RAND() * 1000000),
-	SUBSTRING(CONVERT(NVARCHAR(32), HashBytes('MD5', STR(RAND() * 1000000)), 2), 1, 20),
-	DATEADD(day, FLOOR(RAND() * 365 * 20), '2000-01-01');
+	  FLOOR(RAND() * 1000000),
+	  SUBSTRING(CONVERT(NVARCHAR(32), HashBytes('MD5', STR(RAND() * 1000000)), 2), 1, 20),
+	  DATEADD(day, FLOOR(RAND() * 365 * 20), '2000-01-01');
 
 	SET @loop_counter = @loop_counter + 1;
   END;
@@ -50,12 +54,13 @@ END;
 Call the procedure
 */
 
-
+--SQL 04
 EXECUTE generate_sample_data 1000000;
 
 /*
 Use a CTE
 */
+
 WITH dummy_rows AS (
   SELECT 1 AS id
   UNION ALL
@@ -68,12 +73,13 @@ FROM dummy_rows;
 
 /* This can only generate up to 32,767 rows due to the max of MAXRECURSION */
 
+--SQL 05
 WITH dummy_rows AS (
   SELECT 1 AS id
   UNION ALL
   SELECT id + 1
   FROM dummy_rows
-  WHERE id < 10000
+  WHERE id < 10
 )
 INSERT INTO test_data (test_num, test_text, test_datetime)
 SELECT
@@ -87,6 +93,7 @@ OPTION (MAXRECURSION 1000001);
 Try SELECT TOP with an inbuilt table
 */
 
+--SQL 06
 WITH dummy_rows_a AS (
   SELECT TOP 1000 object_id
   FROM sys.all_columns
