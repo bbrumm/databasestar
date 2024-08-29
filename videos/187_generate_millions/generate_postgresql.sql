b3f73bc25befe6c771574980239b912395d8514b
Ben Brumm <ben@databasestar.com>
2024-08-29 15:36:49 +1000

    Postgres changes

--- videos/187_generate_millions/generate_postgresql.sql
+++ videos/187_generate_millions/generate_postgresql.sql
@@ -1,3 +1,4 @@
+/* SQL 01 */
 CREATE TABLE test_data (
   test_num INTEGER,
   test_text TEXT,
@@ -9,20 +10,14 @@ FROM test_data;
 
 TRUNCATE test_data;
 
-/*
- * Insert using generate_series
- */
-INSERT INTO test_data (test_num, test_text, test_datetime)
+/* SQL 02 */
 SELECT 
-FLOOR(RANDOM() * 1000000) , 
+FLOOR(RANDOM() * 1000000), 
 SUBSTRING(md5(random()::text), 1, 20), 
-'2000-01-01'::date + trunc(random() * 365 * 20)::int
-FROM (
-	SELECT generate_series(1,1000000)
-) s;
-
+'2000-01-01'::date + trunc(random() * 365 * 20)::int;
 
 /*
+SQL 03
 Stored proc
 */
 CREATE OR REPLACE PROCEDURE generate_sample_data(
@@ -40,7 +35,9 @@ BEGIN
    END LOOP;
 END; $$
 
+/* SQL 04 */
 CALL generate_sample_data(1000000);
+
 
 /*
 Recursive CTE
@@ -55,7 +52,7 @@ WITH RECURSIVE dummy_rows(id) AS (
 SELECT id
 FROM dummy_rows;
 
-
+/* SQL 05 */
 INSERT INTO test_data (test_num, test_text, test_datetime)
 WITH RECURSIVE dummy_rows(id) AS (
   SELECT 1 AS id
@@ -70,7 +67,10 @@ SUBSTRING(md5(random()::text), 1, 20),
 '2000-01-01'::date + trunc(random() * 365 * 20)::int
 FROM dummy_rows;
 
+
+
 /*
+SQL 06
 CTE with a cross join
 */
 INSERT INTO test_data (test_num, test_text, test_datetime)
@@ -90,4 +90,24 @@ SUBSTRING(md5(random()::text), 1, 20),
 '2000-01-01'::date + trunc(random() * 365 * 20)::int
 FROM dummy_rows_a
 CROSS JOIN dummy_rows_b;
+
+/* SQL 07 */
+SELECT generate_series(1,10);
+
+
+
+/*
+SQL 08
+ * Insert using generate_series
+ */
+INSERT INTO test_data (test_num, test_text, test_datetime)
+SELECT 
+FLOOR(RANDOM() * 1000000) , 
+SUBSTRING(md5(random()::text), 1, 20), 
+'2000-01-01'::date + trunc(random() * 365 * 20)::int
+FROM (
+	SELECT generate_series(1,1000000)
+) s;
+
+
 
