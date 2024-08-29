9af1b9a3fe54aafe34bbf0b113ef69bc5001a3fc
Ben Brumm <ben@databasestar.com>
2024-08-29 15:09:17 +1000

    Updates for MySQL

--- videos/187_generate_millions/generate_mysql.sql
+++ videos/187_generate_millions/generate_mysql.sql
@@ -1,17 +1,24 @@
+/* SQL 01 */
 CREATE TABLE test_data (
   test_num INT,
   test_text VARCHAR(100),
   test_datetime TIMESTAMP
 );
 
-#This works. Next, wrap this in a procedure with a loop
+/* SQL 02 */
+SELECT
+FLOOR(RAND() * 1000000),
+substring(MD5(RAND()),1,20),
+DATE_ADD('2000-01-01 00:00:00', INTERVAL 1 +  FLOOR(RAND() * 365 * 20) DAY);
 
-#Insert a single row
+/*FROM_UNIXTIME(UNIX_TIMESTAMP('2000-01-01 01:00:00') + FLOOR(RAND() * 60 * 60 * 24 * 365 * 20))*/
+
+/* Insert a single row */
 INSERT INTO test_data (test_num, test_text, test_datetime)
 VALUES (
 FLOOR(RAND() * 1000000),
 substring(MD5(RAND()),1,20),
-FROM_UNIXTIME(UNIX_TIMESTAMP('2000-01-01 01:00:00') + FLOOR(RAND() * 60 * 60 * 24 * 365 * 20))
+DATE_ADD('2000-01-01 00:00:00', INTERVAL 1 +  FLOOR(RAND() * 365 * 20) DAY)
 );
 
 SELECT *
@@ -20,8 +27,9 @@ FROM test_data;
 TRUNCATE test_data;
 
 
-#Create a procedure to run it many times
-#highlight all of this code and run it
+/* Create a procedure to run it many times
+ highlight all of this code and run it */
+ /* SQL 03 */
 DELIMITER $$
 CREATE PROCEDURE generate_sample_data(
   IN number_of_rows INT
@@ -33,18 +41,17 @@ BEGIN
 	VALUES (
 	FLOOR(RAND() * 1000000),
 	substring(MD5(RAND()),1,20),
-	FROM_UNIXTIME(UNIX_TIMESTAMP('2000-01-01 01:00:00') + FLOOR(RAND() * 60 * 60 * 24 * 365 * 20))
+	DATE_ADD('2000-01-01 00:00:00', INTERVAL 1 +  FLOOR(RAND() * 365 * 20) DAY)
 	);
     SET loop_increment = loop_increment + 1;
   END WHILE;
 END$$
 DELIMITER ;
 
-#Run the procedure with different numbers
+/* Run the procedure with different numbers */
 CALL generate_sample_data(1000000);
 
-#What about a recursive CTE?
-#This selects the values from 1-10
+/* What about a recursive CTE? */
 WITH RECURSIVE dummy_rows(id) AS (
   SELECT 1 AS id
   UNION ALL
@@ -55,8 +62,10 @@ WITH RECURSIVE dummy_rows(id) AS (
 SELECT id
 FROM dummy_rows;
 
-#Update the CTE to generate an INSERT statement
-#Try this for different volumes
+/* Update the CTE to generate an INSERT statement
+#Try this for different volumes */
+
+/* SQL 04 */
 INSERT INTO test_data (test_num, test_text, test_datetime)
 WITH RECURSIVE dummy_rows(id) AS (
   SELECT 1 AS id
@@ -68,10 +77,11 @@ WITH RECURSIVE dummy_rows(id) AS (
 SELECT
 FLOOR(RAND() * 1000000),
 substring(MD5(RAND()),1,20),
-FROM_UNIXTIME(UNIX_TIMESTAMP('2000-01-01 01:00:00') + FLOOR(RAND() * 60 * 60 * 24 * 365 * 20))
+DATE_ADD('2000-01-01 00:00:00', INTERVAL 1 +  FLOOR(RAND() * 365 * 20) DAY)
 FROM dummy_rows;
 
-SET SESSION cte_max_recursion_depth=10000000;
+/* SQL 05 */
+SET SESSION cte_max_recursion_depth=10000001;
 
 /*
 CTE with Cross Join
@@ -81,6 +91,7 @@ FROM information_schema.columns;
 
 SELECT * FROM information_schema.tables;
 
+/* SQL 06 */
 INSERT INTO test_data (test_num, test_text, test_datetime)
 WITH dummy_rows_a  AS (
   SELECT 1
@@ -95,7 +106,7 @@ dummy_rows_b AS (
 SELECT
 FLOOR(RAND() * 1000000),
 substring(MD5(RAND()),1,20),
-FROM_UNIXTIME(UNIX_TIMESTAMP('2000-01-01 01:00:00') + FLOOR(RAND() * 60 * 60 * 24 * 365 * 20))
+DATE_ADD('2000-01-01 00:00:00', INTERVAL 1 +  FLOOR(RAND() * 365 * 20) DAY)
 FROM dummy_rows_a
 CROSS JOIN dummy_rows_b;
 
