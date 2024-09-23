1f778730e4b0246861586dcf2ba8f79ba77e5e1c
Ben Brumm <ben@databasestar.com>
2024-09-23 14:53:50 +1000

    Added module 5 exercises

--- courses/course_postgres_mastery/exercises/exercises_module_5.sql
+++ courses/course_postgres_mastery/exercises/exercises_module_5.sql
@@ -3,3 +3,36 @@ Exercises from the PostgreSQL Mastery course
 Module 5
 */
 
+/* 34-01 */
+
+SELECT
+h.history_id,
+h.order_id,
+h.status_id,
+h.status_date,
+s.status_value 
+FROM order_history h
+INNER JOIN order_status s ON h.status_id = s.status_id 
+WHERE order_id = 5468;
+
+/* 34-02 */
+
+CREATE TABLE order_history_range (
+  history_id INTEGER,
+  order_id INTEGER,
+  status_id INTEGER,
+  status_range DATERANGE
+);
+
+INSERT INTO order_history_range (history_id, order_id, status_id, status_range) VALUES
+(5466, 5468, 1, '[2023-08-24, 2023-08-25)'),
+(12028, 5468, 2, '[2023-08-25, 2023-08-26)'),
+(17352, 5468, 3, '[2023-08-26, 2023-08-31)'),
+(21643, 5468, 4, '[2023-08-31, 9999-12-12)');
+
+SELECT
+history_id,
+order_id,
+status_id,
+status_range
+FROM order_history_range;
