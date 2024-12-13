8a2fc1a2714ad17880b6b340b1f32a9fc759d0e2
Ben Brumm <ben@databasestar.com>
2024-12-13 09:22:52 +0000

    Added unpivot

---
+++ courses/course_oracle_mastery/20_unpivot.sql
@@ -0,0 +1,58 @@
+/*
+Unpivot
+*/
+
+--SQL 01
+CREATE TABLE country_summary (
+  country_name VARCHAR2(200),
+  orders_2021 INTEGER,
+  orders_2022 INTEGER,
+  orders_2023 INTEGER,
+  orders_2024 INTEGER
+);
+
+--SQL 02
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Afghanistan', 7, 16, 14, 5);
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Albania', 6, 16, 4, 6);
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Argentina', 16, 27, 28, 6);
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Armenia', 23, 35, 33, 18);
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Australia', 2, 1, 3, 3);
+INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+VALUES ('Azerbaijan', 6, 16, 16, 5);
+
+--SQL 03
+SELECT *
+FROM country_summary;
+
+--SQL 04
+--Using UNPIVOT
+SELECT *
+FROM country_summary
+UNPIVOT (
+  total_orders
+  FOR order_year
+  IN (orders_2021, orders_2022, orders_2023, orders_2024)
+);
+
+
+
+--SQL 05
+--Rename order_year values
+SELECT *
+FROM country_summary
+UNPIVOT (
+  total_orders
+  FOR order_year
+  IN (
+    orders_2021 AS 2021, 
+    orders_2022 AS 2022, 
+    orders_2023 AS 2023, 
+    orders_2024 AS 2024
+  )
+);
+
