9dacc42e8f8057e4be738113368111643e8cc2c0
Ben Brumm <ben@databasestar.com>
2024-12-13 14:48:46 +0000

    Added json scripts

---
+++ courses/course_oracle_mastery/35_json_objects.sql
@@ -0,0 +1,19 @@
+/*
+JSON Objects
+*/
+
+--SQL 01
+INSERT INTO test_json (id, some_data)
+VALUES (2, '{"color":"blue", "size":"large"}');
+
+--SQL 02
+SELECT id, some_data
+FROM test_json;
+
+--SQL 03
+INSERT INTO test_json (id, some_data)
+VALUES (3, '{"color":"white", "in_stock":"true"}');
+
+--SQL 04
+INSERT INTO test_json (id, some_data)
+VALUES (4, '{"color":"white", "in_stock":"false", "supplier":{"supplier_name":"ABC Corp", "email":"orders@abccorp.com"}}');
