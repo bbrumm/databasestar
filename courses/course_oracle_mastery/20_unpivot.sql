ae5c1b991a98e91f76230c13729ae2f4e5cf7831
Ben Brumm <ben@databasestar.com>
2025-02-10 20:19:17 +0000

    Updated unpivot

--- courses/course_oracle_mastery/20_unpivot.sql
+++ courses/course_oracle_mastery/20_unpivot.sql
@@ -5,24 +5,24 @@ Unpivot
 --SQL 01
 CREATE TABLE country_summary (
   country_name VARCHAR2(200),
-  orders_2021 INTEGER,
   orders_2022 INTEGER,
   orders_2023 INTEGER,
-  orders_2024 INTEGER
+  orders_2024 INTEGER,
+  orders_2025 INTEGER
 );
 
 --SQL 02
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Afghanistan', 7, 16, 14, 5);
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Albania', 6, 16, 4, 6);
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Argentina', 16, 27, 28, 6);
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Armenia', 23, 35, 33, 18);
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Australia', 2, 1, 3, 3);
-INSERT INTO country_summary (country_name, orders_2021, orders_2022, orders_2023, orders_2024)
+INSERT INTO country_summary (country_name, orders_2022, orders_2023, orders_2024, orders_2025)
 VALUES ('Azerbaijan', 6, 16, 16, 5);
 
 --SQL 03
@@ -36,7 +36,7 @@ FROM country_summary
 UNPIVOT (
   total_orders
   FOR order_year
-  IN (orders_2021, orders_2022, orders_2023, orders_2024)
+  IN (orders_2022, orders_2023, orders_2024, orders_2025)
 );
 
 
@@ -49,10 +49,10 @@ UNPIVOT (
   total_orders
   FOR order_year
   IN (
-    orders_2021 AS 2021, 
     orders_2022 AS 2022, 
     orders_2023 AS 2023, 
-    orders_2024 AS 2024
+    orders_2024 AS 2024,
+    orders_2025 AS 2025
   )
 );
 
