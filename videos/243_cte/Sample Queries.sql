1740e828ccabf527558562115e36f7653dd98b5a
Ben Brumm <ben@databasestar.com>
2025-07-27 19:20:34 +0100

    More queries from videos

--- videos/243_cte/Sample Queries.sql
+++ videos/243_cte/Sample Queries.sql
@@ -1,1 +1,76 @@
-CREATE TABLE 
+CREATE TABLE customers (
+    id INTEGER,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    CONSTRAINT pk_customers PRIMARY KEY (id)
+);
+
+CREATE TABLE orders (
+    id INTEGER,
+    customer_id INTEGER,
+    order_total INTEGER,
+    CONSTRAINT pk_orders PRIMARY KEY (id),
+    CONSTRAINT fk_order_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customers (id)
+);
+
+INSERT INTO customers (id, first_name, last_name) VALUES
+(1, 'Sarah', 'Jones'),
+(2, 'Mark', 'Williamson'),
+(3, 'Tony', 'Stark');
+
+INSERT INTO orders (id, customer_id, order_total) VALUES
+(1, 2, 510),
+(2, 3, 100),
+(3, 1, 300),
+(4, 1, 800),
+(5, 3, 650),
+(6, 2, 200),
+(7, 3, 180);
+
+
+--Subquery
+SELECT *
+FROM customers
+WHERE id IN (
+  SELECT customer_id
+  FROM orders
+  GROUP BY customer_id
+  HAVING SUM(order_total) > 1000
+);
+
+--View
+DROP VIEW top_customers;
+
+CREATE VIEW top_customers AS
+SELECT customers.id,
+       customers.first_name,
+       customers.last_name,
+       SUM(order_total) AS total_spent
+FROM orders
+INNER JOIN customers ON orders.customer_id = customers.id
+GROUP BY customers.id,
+       customers.first_name,
+       customers.last_name
+HAVING SUM(order_total) > 1000;
+
+SELECT *
+FROM top_customers;
+
+--CTE
+WITH order_totals AS (
+  SELECT customer_id, SUM(order_total) AS total_spent
+  FROM orders
+  GROUP BY customer_id
+)
+SELECT c.id, c.first_name, c.last_name, o.total_spent
+FROM customers c
+JOIN order_totals o ON c.id = o.customer_id
+WHERE o.total_spent > 1000;
+
+/*
+ Subquery: one off, simple logic
+ View: reuse logic across multiple queries
+ CTE: break your query into steps, reuse within a query
+ */
--- videos/255_dirty/Cafe Data Queries.sql
+++ videos/255_dirty/Cafe Data Queries.sql
@@ -46,10 +46,6 @@ SELECT "Price Per Unit", COUNT(*)
 FROM dirty_cafe_sales
 GROUP BY "Price Per Unit";
 
-SELECT "Price Per Unit", COUNT(*)
-FROM dirty_cafe_sales
-GROUP BY "Price Per Unit";
-
 
 SELECT "Total Spent", COUNT(*)
 FROM dirty_cafe_sales
@@ -68,4 +64,193 @@ SELECT "Transaction Date", COUNT(*)
 FROM dirty_cafe_sales
 GROUP BY "Transaction Date";
 
+--Alter column names
+ALTER TABLE dirty_cafe_sales RENAME "Transaction ID" TO transaction_id;
+ALTER TABLE dirty_cafe_sales RENAME "Price Per Unit" TO price_per_unit;
+ALTER TABLE dirty_cafe_sales RENAME "Total Spent" TO total_spent;
+ALTER TABLE dirty_cafe_sales RENAME "Payment Method" TO payment_method;
+ALTER TABLE dirty_cafe_sales RENAME "Transaction Date" TO transaction_date;
 
+
+
+UPDATE dirty_cafe_sales
+SET payment_method = NULL
+WHERE payment_method IN ('ERROR', 'UNKNOWN');
+
+UPDATE dirty_cafe_sales
+SET location = NULL
+WHERE location IN ('ERROR', 'UNKNOWN');
+
+UPDATE dirty_cafe_sales
+SET transaction_date = NULL
+WHERE transaction_date IN ('ERROR', 'UNKNOWN');
+
+
+SELECT
+column_name,
+data_type,
+ordinal_position
+FROM information_schema.columns
+WHERE table_name = 'dirty_cafe_sales';
+
+--Update data types
+ALTER TABLE dirty_cafe_sales
+ADD COLUMN transaction_date_new DATE;
+
+SELECT transaction_id, transaction_date
+FROM dirty_cafe_sales;
+
+SELECT transaction_id,
+       transaction_date,
+       TO_DATE(transaction_date, 'YYYY-MM-DD')
+FROM dirty_cafe_sales;
+
+UPDATE dirty_cafe_sales
+SET transaction_date_new = TO_DATE(transaction_date, 'YYYY-MM-DD');
+
+SELECT * FROM dirty_cafe_sales;
+
+ALTER TABLE dirty_cafe_sales
+DROP COLUMN transaction_date;
+
+ALTER TABLE dirty_cafe_sales
+RENAME transaction_date_new TO transaction_date;
+
+SELECT * FROM dirty_cafe_sales;
+
+SELECT *
+FROM dirty_cafe_sales
+WHERE (quantity IS NOT NULL AND quantity NOT IN ('ERROR', 'UNKNOWN'))
+AND (price_per_unit IS NOT NULL AND price_per_unit NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NULL OR total_spent IN ('ERROR', 'UNKNOWN'));
+
+
+UPDATE dirty_cafe_sales
+SET total_spent = quantity * price_per_unit
+WHERE (quantity IS NOT NULL AND quantity NOT IN ('ERROR', 'UNKNOWN'))
+AND (price_per_unit IS NOT NULL AND price_per_unit NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NULL OR total_spent IN ('ERROR', 'UNKNOWN'));
+
+
+UPDATE dirty_cafe_sales
+SET total_spent = CAST(quantity AS DECIMAL) * CAST(price_per_unit AS DECIMAL)
+WHERE (quantity IS NOT NULL AND quantity NOT IN ('ERROR', 'UNKNOWN'))
+AND (price_per_unit IS NOT NULL AND price_per_unit NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NULL OR total_spent IN ('ERROR', 'UNKNOWN'));
+
+
+SELECT *
+FROM dirty_cafe_sales
+WHERE (quantity IS NOT NULL AND quantity NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NOT NULL AND total_spent NOT IN ('ERROR', 'UNKNOWN'))
+AND (price_per_unit IS NULL OR price_per_unit IN ('ERROR', 'UNKNOWN'));
+
+UPDATE dirty_cafe_sales
+SET price_per_unit = CAST(total_spent AS DECIMAL) / CAST(quantity AS DECIMAL)
+WHERE (quantity IS NOT NULL AND quantity NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NOT NULL AND total_spent NOT IN ('ERROR', 'UNKNOWN'))
+AND (price_per_unit IS NULL OR price_per_unit IN ('ERROR', 'UNKNOWN'));
+
+SELECT *
+FROM dirty_cafe_sales
+WHERE (price_per_unit IS NOT NULL AND price_per_unit NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NOT NULL AND total_spent NOT IN ('ERROR', 'UNKNOWN'))
+AND (quantity IS NULL OR quantity IN ('ERROR', 'UNKNOWN'));
+
+
+UPDATE dirty_cafe_sales
+SET quantity = CAST(total_spent AS DECIMAL) / CAST(price_per_unit AS DECIMAL)
+WHERE (price_per_unit IS NOT NULL AND price_per_unit NOT IN ('ERROR', 'UNKNOWN'))
+AND (total_spent IS NOT NULL AND total_spent NOT IN ('ERROR', 'UNKNOWN'))
+AND (quantity IS NULL OR quantity IN ('ERROR', 'UNKNOWN'));
+
+SELECT *
+FROM dirty_cafe_sales;
+
+SELECT quantity, COUNT(*)
+FROM dirty_cafe_sales
+GROUP BY quantity;
+
+SELECT price_per_unit, COUNT(*)
+FROM dirty_cafe_sales
+GROUP BY price_per_unit;
+
+
+SELECT total_spent, COUNT(*)
+FROM dirty_cafe_sales
+GROUP BY total_spent;
+
+UPDATE dirty_cafe_sales
+SET quantity = NULL
+WHERE quantity IN ('ERROR', 'UNKNOWN');
+
+UPDATE dirty_cafe_sales
+SET price_per_unit = NULL
+WHERE price_per_unit IN ('ERROR', 'UNKNOWN');
+
+UPDATE dirty_cafe_sales
+SET total_spent = NULL
+WHERE total_spent IN ('ERROR', 'UNKNOWN');
+
+ALTER TABLE dirty_cafe_sales
+ALTER COLUMN quantity TYPE INTEGER;
+
+ALTER TABLE dirty_cafe_sales
+ALTER COLUMN quantity TYPE INTEGER USING quantity::integer;
+
+UPDATE dirty_cafe_sales
+SET quantity = '1'
+WHERE quantity = '1.00000000000000000000';
+
+UPDATE dirty_cafe_sales
+SET quantity = '2'
+WHERE quantity = '2.0000000000000000';
+
+UPDATE dirty_cafe_sales
+SET quantity = '3'
+WHERE quantity = '3.0000000000000000';
+
+UPDATE dirty_cafe_sales
+SET quantity = '4'
+WHERE quantity = '4.0000000000000000';
+
+UPDATE dirty_cafe_sales
+SET quantity = '5'
+WHERE quantity = '5.0000000000000000';
+
+
+SELECT price_per_unit, COUNT(*)
+FROM dirty_cafe_sales
+GROUP BY price_per_unit;
+
+UPDATE dirty_cafe_sales
+SET price_per_unit = NULL
+WHERE price_per_unit IN ('ERROR', 'UNKNOWN');
+
+ALTER TABLE dirty_cafe_sales
+ALTER COLUMN price_per_unit TYPE DECIMAL USING price_per_unit::decimal;
+
+
+SELECT total_spent, COUNT(*)
+FROM dirty_cafe_sales
+GROUP BY total_spent;
+
+
+UPDATE dirty_cafe_sales
+SET total_spent = NULL
+WHERE total_spent IN ('ERROR', 'UNKNOWN');
+
+ALTER TABLE dirty_cafe_sales
+ALTER COLUMN total_spent TYPE DECIMAL USING total_spent::decimal;
+
+--Final query
+SELECT
+transaction_id,
+item,
+quantity,
+price_per_unit,
+total_spent,
+payment_method,
+location,
+transaction_date
+FROM dirty_cafe_sales;
